c7267e6d506454086e6e0e59e485aaf8c9f0e3c4
Mirko Grpß <mirkodietmargross@gmail.com>
2017-09-14 10:25:38 +0200

    initial Commit

---
+++ src/com/sabel/farbenspiele/Farbenspiel.java
@@ -0,0 +1,11 @@
+package com.sabel.farbenspiele;
+
+import sun.applet.Main;
+
+public class Farbenspiel {
+
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+    }
+
+}
---
+++ src/com/sabel/farbenspiele/MainWindow.java
@@ -0,0 +1,56 @@
+package com.sabel.farbenspiele;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow extends JFrame {
+
+    private JPanel jpanel;
+    private JButton jbtnGreen;
+    private JButton jbtnYellow;
+    private JButton jbtnBlue;
+
+    public MainWindow() throws HeadlessException {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.initComponents();
+        this.setVisible(true);
+    }
+
+    private void initComponents()
+    {
+        this.jpanel = new JPanel();
+        this.jbtnBlue = new JButton("Blau");
+
+        jbtnBlue.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                jpanel.setBackground(Color.BLUE);
+            }
+        });
+
+        this.jbtnGreen = new JButton("Grün");
+        jbtnGreen.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                jpanel.setBackground(Color.GREEN);
+            }
+        });
+        this.jbtnYellow = new JButton("Gelb");
+        jbtnYellow.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                jpanel.setBackground(Color.YELLOW);
+            }
+        });
+        this.jpanel.add(jbtnBlue);
+        this.jpanel.add(jbtnGreen);
+        this.jpanel.add(jbtnYellow);
+        this.add(jpanel);
+    }
+}
